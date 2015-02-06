748e0ab7e5f27f43a15065e1e082927e24a678a4
elvis1615 <elvis_rolando@hotmail.es>
2015-02-06 10:25:52 -0600

    hola mundo

--- prubea/src/prubea/Prubea.java
+++ prubea/src/prubea/Prubea.java
@@ -16,6 +16,8 @@ public class Prubea {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("ola k ase");
+        
     }
     
 }
